49435a8c504d1333e836e97515c707231b4b22e8
ChoriaKiinweill <2963482580@qq.com>
2026-03-06 17:11:58 +0800

    update the main page

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,15 +1,12 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="flex min-h-sreen flex-col">
+    <AppNavBar />
     <main>
       <slot />
     </main>
   </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,19 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1>我是一个傻逼</h1>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Travel Log</h1>
+        <p class="py-6">
+          Use this travel log app to keep track of your travel itinerary.
+        </p>
+        <button class="btn btn-accent">
+          Sign In With GitHub<Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
